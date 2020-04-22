9cc8e4beb6359ccc73927cfcfefae1b77d8968c5
ubuntu <ubuntu@ip-172-31-95-24.ec2.internal>
2020-04-22 04:20:55 +0000

    Response entity changes

--- userservice/src/main/java/com/stackroute/userservice/controller/UserController.java
+++ userservice/src/main/java/com/stackroute/userservice/controller/UserController.java
@@ -6,6 +6,8 @@ package com.stackroute.userservice.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,24 +23,33 @@ import com.stackroute.userservice.service.IUserService;
  * @author ubuntu
  * 
  */
- 
+
 @RestController
 @CrossOrigin(origins = "*")
-public class UserController {  
+public class UserController {
 
 	@Autowired
 	IUserService userService;
 
 	@PostMapping("/user")
-	public ApiResponse saveUser(@RequestBody UserData userData) {
-		return userService.saveUser(userData);
+	public ResponseEntity<ApiResponse> saveUser(@RequestBody UserData userData) {
+		ResponseEntity<ApiResponse> responseEntity = null;
+		ApiResponse apiResponse = userService.saveUser(userData);
+		if (apiResponse != null) {
+			if (apiResponse.getHttpStatus() == 200) {
+				responseEntity = new ResponseEntity(apiResponse, HttpStatus.OK);
+			} else if (apiResponse.getHttpStatus() == 400) {
+				responseEntity = new ResponseEntity(apiResponse, HttpStatus.BAD_REQUEST);
+			}
+		}
+		return responseEntity;
 	}
 
 	@RequestMapping("/login")
 	public UserResponse authenticateUser(HttpServletRequest request) {
 		return userService.authenticateUser(request);
 	}
-	
+
 	@RequestMapping("/logout")
 	public void logout(HttpServletRequest request) {
 		request.getSession().invalidate();
